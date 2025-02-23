27031d2d6e1eca8149dab497312c185b931922a5
Dmitri Polchinski <meetree@ya.ru>
2025-02-24 03:37:25 +0400

    refactor: simplify script for integration tests (examples)

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -2,26 +2,41 @@
 
 set -euo pipefail
 
-file="examples/$1.b"
-expected=$2
-input=${3:-}
+declare -A examples=(
+  ['helloworld']='Hello World!'
+  ['addition']='7'
+  ['bubblesort:978365412']='123456789'
+  ['bubblesort:aZefcYbdX']='XYZabcdef'
+  ['rot13:Uryyb, Jbeyq!']='Hello, World!'
+  ['rot13:Hello, World!']='Uryyb, Jbeyq!'
+)
 
-if [ ! -f "$file" ]; then
-  echo "error: file $file doesn't exist"
-  exit 1
-fi
+for example in "${!examples[@]}"; do
+  name="$example"
+  input=""
+  expected="${examples[$example]}"
 
-if [ -n "$input" ]; then
+  if [[ $example == *:* ]]; then
+    name="${example%%:*}"
+    input="${example##*:}"
+  fi
+
+  file="examples/$name.b"
+
+  if [ ! -f "$file" ]; then
+    echo "error: File $file doesn't exist"
+    exit 1
+  fi
+
   output=$(echo -n "$input" | scala-cli run . -q -- "$(< "$file")")
-else
-  output=$(scala-cli run . -q -- "$(< "$file")")
-fi
 
-if [ "$output" = "$expected" ]; then
-  echo "✓ $file passed"
-else
-  echo "✗ $file failed"
-  echo "  expected: '$expected'"
-  echo "  got:      '$output'"
-  exit 1
-fi
+  echo "Running $file $input"
+  if [ "$output" = "$expected" ]; then
+    echo "✓ $file passed"
+  else
+    echo "✗ $file failed"
+    echo "  expected: '$expected'"
+    echo "  got:      '$output'"
+    exit 1
+  fi
+done
