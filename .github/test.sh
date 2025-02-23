1026ed5116e7d5535bf6f83c381ae4baaf422b97
Dmitri Polchinski <meetree@ya.ru>
2025-02-23 22:14:33 +0400

    ci: add examples matrix

---
+++ .github/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+file="examples/$1.b"
+expected=$2
+input=${3:-}
+
+if [ ! -f "$file" ]; then
+  echo "error: file $file doesn't exist"
+  exit 1
+fi
+
+if [ -n "$input" ]; then
+  output=$(echo -n "$input" | scala-cli run . -q -- "$(cat "$file")")
+else
+  output=$(scala-cli run . -q -- "$(cat "$file")")
+fi
+
+if [ "$output" = "$expected" ]; then
+  echo "✓ $file passed"
+else
+  echo "✗ $file failed"
+  echo "  expected: '$expected'"
+  echo "  got:      '$output'"
+  exit 1
+fi
